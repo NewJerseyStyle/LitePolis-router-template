e85863d4f70004d8419cce33f6ad0d0271467645
David H. <22520563+NewJerseyStyle@users.noreply.github.com>
2025-03-03 15:19:21 -0500

    added file structure

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # LitePolis-database-template
 Template repository to build database module for LitePolis
+
+things you must contain for a database router
+
+important interfaces
+
+get started
---
+++ litepolis_router_database/core.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel
+from fastapi import APIRouter
+
+router = APIRouter()
+prefix = __name__.split('.')[-2]
+dependencies = []
+
+tags_metadata = [
+    {
+        "name": "User",
+        "description": "Operations related to the authenticated user",
+    }
+]
+
+class UserResponseMessage(BaseModel):
+    id: int
+    email: str
+    role: str
+
+class ResponseMessage(BaseModel):
+    detail: Union[str,
+                  UserResponseMessage]
+    error: str | None = None
+    message: str | None = None
+    status_code: int = 200
+
+@router.get("/")
+async def get_testroute():
+    return ResponseMessage(detail="OK")
+
+@router.get("/user", tags=["User"], response_model=ResponseMessage)
+async def get_user_route():
+    """This endpoint returns information about the currently authenticated user."""
+    return ResponseMessage(
+        message="User information",
+        detail=UserResponseMessage(
+            id=0,
+            email='user@example.com',
+            role='user'
+        )
+    )
