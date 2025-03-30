5411a1f290cd393789804092c82b878952717156
David H. <22520563+NewJerseyStyle@users.noreply.github.com>
2025-03-30 10:15:42 -0400

    universial test cases with enforcer for key attributes

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,28 +1,36 @@
+import unittest
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
-from litepolis_router_template import *
+from importlib import import_module
+from utils import find_package_name
 
-app = FastAPI()
+class TestCore(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.pkg_name = find_package_name()
+        cls.core = import_module(f"{cls.pkg_name}")
+        cls.router = cls.core.router
+        cls.prefix = cls.core.prefix
 
-app.include_router(
-    router,
-    prefix=f"/api/{prefix}"
-)
+        cls.app = FastAPI()
+        cls.app.include_router(
+            cls.router,
+            prefix=f"/api/{cls.prefix}"
+        )
+        cls.client = TestClient(cls.app)
 
-client = TestClient(app)
+    def test_read_main(self):
+        response = self.client.get(f"/api/{self.prefix}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["detail"], "OK")
 
-def test_read_main():
-    response = client.get(f"/api/{prefix}")
-    assert response.status_code == 200
-    assert response.json()["detail"] == "OK"
-
-def test_read_user():
-    response = client.get(f"/api/{prefix}/user")
-    assert response.status_code == 200
-    json_dict = response.json()
-    assert json_dict["message"] == "User information"
-    assert json_dict["detail"] == {
-        "id": 0,
-        "email": "user@example.com",
-        "role": "user"
-    }
+    def test_read_user(self):
+        response = self.client.get(f"/api/{self.prefix}/user")
+        self.assertEqual(response.status_code, 200)
+        json_dict = response.json()
+        self.assertEqual(json_dict["message"], "User information")
+        self.assertEqual(json_dict["detail"], {
+            "id": 0,
+            "email": "user@example.com",
+            "role": "user"
+        })
